6bfca8a94ac4b609f475b0d2cda81ead0434a608
Alexandr <alexresh62@yandex.ru>
2023-08-04 16:34:23 +0300

    Added gamemode switch by packet (serverside)

---
+++ src/main/java/ru/mcflightcraft/FlightCraftMain.java
@@ -0,0 +1,36 @@
+package ru.mcflightcraft;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.GameMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FlightCraftMain implements ModInitializer {
+	public static final Identifier gamemodePacket = new Identifier("flightcraft", "gamemode_change_packet");
+    public static final Logger LOGGER = LoggerFactory.getLogger("flightcraft");
+
+	@Override
+	public void onInitialize() {
+		ServerPlayNetworking.registerGlobalReceiver(gamemodePacket,(server, player, handler, buf, responseSender) -> {
+			int gamemode = buf.readInt();
+			if(!player.hasPermissionLevel(2)){
+				player.sendMessage(Text.literal("No permission!").formatted(Formatting.BOLD).formatted(Formatting.RED), true);
+				return;
+			}
+			server.execute(()->{
+				switch (gamemode) {
+					case 0 -> player.changeGameMode(GameMode.SURVIVAL);
+					case 1 -> player.changeGameMode(GameMode.CREATIVE);
+					case 2 -> player.changeGameMode(GameMode.ADVENTURE);
+					case 3 -> player.changeGameMode(GameMode.SPECTATOR);
+					default -> {}
+				}
+			});
+		});
+	}
+}
